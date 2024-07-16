281b12d97825140f1b3a194743d24d32c25e427d
Hailey <haileyacarlson@gmail.com>
2024-07-15 21:11:03 -0700

    Added comments to load_factor.py

--- airflow/plugins/final_project_operators/load_fact.py
+++ airflow/plugins/final_project_operators/load_fact.py
@@ -4,8 +4,16 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
+    """
+    Operator to stage data from S3 to Redshift.
+    """
+    # UI color for the Airflow web interface
     ui_color = '#358140'
+
+    # Template fields for dynamic rendering
     template_fields = ("s3_key",)
+
+    # SQL COPY command template
     copy_sql = """ 
         COPY {}
         FROM '{}'
@@ -17,15 +25,17 @@ class StageToRedshiftOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 redshift_conn_id = "",
-                 aws_credentials_id = "",
-                 table = "",
-                 s3_bucket = "",
-                 s3_key = "",
-                 region = "",
-                 data_format = "",
+                 redshift_conn_id = "", # Redshift connection ID
+                 aws_credentials_id = "", # AWS credentials ID
+                 table = "", # Target table in Redshift
+                 s3_bucket = "", # S3 bucket name
+                 s3_key = "", # S3 key prefix
+                 region = "", # AWS region
+                 data_format = "", # Data format (e.g., JSON, SCV)
                  *args, **kwargs):
-
+        """
+        Initialize the StageToRedshiftOperator.
+        """
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.aws_credentials_id = aws_credentials_id
@@ -36,16 +46,23 @@ class StageToRedshiftOperator(BaseOperator):
         self.data_format = data_format
 
     def execute(self, context):
+        """
+        Execute the data staging process.
+        """
         self.log.info('Executing StageToRedshiftOperator')
 
+        # Get AWS credentials
         aws_hook = AwsHook(self.aws_credentials_id)
         credentials = aws_hook.get_credentials()
+
+        # Establish connection to the Redshift database
         redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id)
 
+        # Clear the target table in Redshift
         self.log.info("Clearing data from Redshift table")
         redshift.run("DELETE FROM {}".format(self.table))
 
-
+        # Copy data from S3 to Redshift
         self.log.info("Copying data from S3 to Redshift")
         rendered_key = self.s3_key.format(**context)
         s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
